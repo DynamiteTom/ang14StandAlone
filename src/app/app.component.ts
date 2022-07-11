2866acc7797f19f1cc5c712868a94abf0ea94e1f
Gordon T Cumming <dynaphoneimages@hotmail.com>
2022-07-11 12:40:13 +0100

    First commit for Ang14StandAlone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  standalone: true,
+  imports: [ CommonModule ],
+  template: '<h1 style="color: blue">Ang14 StandAlone Blue</h1>'
 })
 export class AppComponent {
   title = 'ang14StandAlone';
