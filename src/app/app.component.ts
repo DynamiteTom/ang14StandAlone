097b46d73377950f74a001996288bb6321a224fe
Gordon T Cumming <dynaphoneimages@hotmail.com>
2022-07-11 13:39:28 +0100

    Removed scss and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
 
-@Component({
+import { CommonModule } from '@angular/common';
+import { Component, OnInit} from '@angular/core'; 
+// OnChanges, SimpleChanges } 
+
+@Component({  
   selector: 'app-root',
   standalone: true,
   imports: [ CommonModule ],
-  template: '<h1 style="color: blue">Ang14 StandAlone Blue</h1>'
+  template: '<h1 style="color: blue">Ang14 D StandAlone Blue</h1>'
 })
-export class AppComponent {
-  title = 'ang14StandAlone';
+export class AppComponent implements OnInit{
+  title = 'ang14StandAlone A';
+
+  ngOnInit(){
+
+  }
+/*
+  ngOnChanges(changes: SimpleChanges){
+
+  }
+  */
 }
