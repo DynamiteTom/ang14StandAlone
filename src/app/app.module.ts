adf6bd4b292f934197a74def12e89019131cf2b7
Gordon T Cumming <dynaphoneimages@hotmail.com>
2022-07-12 09:23:32 +0100

    About to remove Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+// import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+// import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
   ],
-  imports: [
-    BrowserModule,
-    AppComponent
+  imports: [  
+//    BrowserModule,
+//    AppComponent
   ],
-  providers: []
+    providers: []
 })
 export class AppModule { }
